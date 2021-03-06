3504908d8d9df7f9ecd2dd23ac74e80dc2c86cee
NicoleBaraldi <79142496+NicoleBaraldi@users.noreply.github.com>
2021-03-06 16:29:35 -0300

    Add files via upload

---
+++ Atividade 3/Cálculo_Triângulos/Cálculo_Triângulos/Cálculo Triângulo.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Cálculo_Triângulos
+{
+    partial class Calculo_Triangulo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculo_Triangulo));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.mska = new System.Windows.Forms.MaskedTextBox();
+            this.mskb = new System.Windows.Forms.MaskedTextBox();
+            this.mskc = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(129, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C";
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Enabled = false;
+            this.txtresultado.Location = new System.Drawing.Point(28, 336);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(221, 23);
+            this.txtresultado.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Resultado";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.Color.Azure;
+            this.btncalcular.Location = new System.Drawing.Point(28, 391);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(91, 31);
+            this.btncalcular.TabIndex = 3;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.BackColor = System.Drawing.Color.Azure;
+            this.btnlimpar.Location = new System.Drawing.Point(288, 391);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(91, 31);
+            this.btnlimpar.TabIndex = 5;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = false;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.BackColor = System.Drawing.Color.Azure;
+            this.btnsair.Location = new System.Drawing.Point(158, 391);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(91, 31);
+            this.btnsair.TabIndex = 4;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = false;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // mska
+            // 
+            this.mska.Location = new System.Drawing.Point(28, 192);
+            this.mska.Mask = "00.00";
+            this.mska.Name = "mska";
+            this.mska.Size = new System.Drawing.Size(100, 23);
+            this.mska.TabIndex = 0;
+            // 
+            // mskb
+            // 
+            this.mskb.Location = new System.Drawing.Point(149, 192);
+            this.mskb.Mask = "00.00";
+            this.mskb.Name = "mskb";
+            this.mskb.Size = new System.Drawing.Size(100, 23);
+            this.mskb.TabIndex = 1;
+            // 
+            // mskc
+            // 
+            this.mskc.Location = new System.Drawing.Point(91, 256);
+            this.mskc.Mask = "00.00";
+            this.mskc.Name = "mskc";
+            this.mskc.Size = new System.Drawing.Size(100, 23);
+            this.mskc.TabIndex = 2;
+            // 
+            // Calculo_Triangulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(596, 450);
+            this.Controls.Add(this.mskc);
+            this.Controls.Add(this.mskb);
+            this.Controls.Add(this.mska);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Lucida Fax", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(614, 497);
+            this.MinimumSize = new System.Drawing.Size(614, 497);
+            this.Name = "Calculo_Triangulo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cálculo Triângulo";
+            this.Load += new System.EventHandler(this.Calculo_Triangulo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.MaskedTextBox mska;
+        private System.Windows.Forms.MaskedTextBox mskb;
+        private System.Windows.Forms.MaskedTextBox mskc;
+    }
+}
+
---
+++ Atividade 3/Cálculo_Triângulos/Cálculo_Triângulos/Cálculo Triângulo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cálculo_Triângulos
+{
+    public partial class Calculo_Triangulo : Form
+    {
+        public Calculo_Triangulo()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            mska.Clear();
+            mskb.Clear();
+
+            mskc.Text = String.Empty;
+
+            txtresultado.Text = "";
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double ladoa, ladob, ladoc;
+
+            if (double.TryParse(mska.Text, out ladoa)&&
+                double.TryParse(mskb.Text, out ladob)&&
+                double.TryParse(mskc.Text, out ladoc))
+            {
+                if ((Math.Abs(ladob - ladoc) < ladoa && ladoa < (ladob + ladoc)) && 
+                    (Math.Abs(ladoa - ladoc) < ladob && ladob < (ladoa + ladoc)) &&
+                    (Math.Abs(ladoa - ladob) < ladoc && ladoc < (ladoa + ladob)))
+                {
+                    if (ladoa == ladob && ladoa == ladoc)
+                        txtresultado.Text = "Triângulo Equilátero";
+                    else if (ladoa == ladob && ladoa != ladoc)
+                        txtresultado.Text = "Triângulo Isosceles";
+                    else
+                        txtresultado.Text = "Triângulo Escaleno";
+                }
+                else
+                {
+                    txtresultado.Text = "Não foi possível formar um triângulo";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Insira novamente os dados","Atenção!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Calculo_Triangulo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
