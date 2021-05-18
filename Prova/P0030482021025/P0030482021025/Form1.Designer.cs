425d44207f85256242841cc209155b9336f6b4d6
Nicole Baraldi <79142496+NicoleBaraldi@users.noreply.github.com>
2021-05-18 12:21:08 -0300

    Add files via upload

---
+++ Prova/P0030482021025/P0030482021025/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace P0030482021025
+{
+    partial class FrmProva
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmProva));
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstExibir = new System.Windows.Forms.ListBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.DeepPink;
+            this.btnVerificar.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.ForeColor = System.Drawing.Color.White;
+            this.btnVerificar.Location = new System.Drawing.Point(27, 533);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(144, 51);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lstExibir
+            // 
+            this.lstExibir.FormattingEnabled = true;
+            this.lstExibir.ItemHeight = 16;
+            this.lstExibir.Location = new System.Drawing.Point(27, 12);
+            this.lstExibir.MaximumSize = new System.Drawing.Size(410, 500);
+            this.lstExibir.MinimumSize = new System.Drawing.Size(410, 500);
+            this.lstExibir.Name = "lstExibir";
+            this.lstExibir.Size = new System.Drawing.Size(410, 500);
+            this.lstExibir.TabIndex = 1;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.DeepPink;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(293, 533);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(144, 51);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // FrmProva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(465, 596);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lstExibir);
+            this.Controls.Add(this.btnVerificar);
+            this.ForeColor = System.Drawing.Color.MistyRose;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(483, 643);
+            this.MinimumSize = new System.Drawing.Size(483, 643);
+            this.Name = "FrmProva";
+            this.Text = "Prova";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstExibir;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Prova/P0030482021025/P0030482021025/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030482021025
+{
+    public partial class FrmProva : Form
+    {
+        public FrmProva()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] Valores = new double[5, 4];
+            double[] TotalMes = new double[5];
+            string valor;
+            double totalMeses = 0;
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    valor = Interaction.InputBox("Favor, digite o valor da semana " + (y + 1) + " Do Mês: " + (x + 1), "Digite os dados");
+                    if (valor == "")
+                    {
+                        MessageBox.Show("O campo está vazio!");
+                        y--;
+                    }
+                    else if (double.TryParse(valor, out Valores[x, y]))
+                    {
+                        double.TryParse(valor, out Valores[x, y]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor, digitar apenas números!");
+                        y--;
+                    }
+                }
+            }
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    TotalMes[x] = TotalMes[x] + Valores[x, y];
+                }
+                totalMeses = totalMeses + TotalMes[x];
+            }
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    lstExibir.Items.Add("Total do Mês: " + (x + 1) + " Semana: " + (y + 1) + ": R$" + Valores[x, y].ToString("N2"));
+                }
+                lstExibir.Items.Add("->Total Mensal: R$" + TotalMes[x].ToString("N2"));
+                lstExibir.Items.Add("--------------------------------------------------------------");
+            }
+            lstExibir.Items.Add("Total Geral: R$" + totalMeses.ToString("N2"));
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lstExibir.Items.Clear();
+        }
+    }
+}
